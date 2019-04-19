6bd4437198be1c98810587c881d3eb28e14578bc
BelikovBR <39759960+BelikovBR@users.noreply.github.com>
2019-04-19 15:51:50 +0300

    Add files via upload
    Откалибровано время задержки для отображения данных с наименьшей задержкой

--- I2Cscanner_for_TCA9gfd548A.ino
+++ I2Cscanner_for_TCA9gfd548A.ino
@@ -3,6 +3,13 @@
      *
      * Based on code c. 2009, Tod E. Kurt, http://todbot.com/blog/
      *
+     *  Исходное положение для корректировки
+     *  Корректировка
+     *  Опрос датчиков 
+     *  v1: после опроса сразу отправлять на сервер
+     *  v2: формировать пакет и отправлять по беспроводной связи
+     *  Отправка
+     *
      */
      #include "I2Cdev.h"
     #include "MPU6050_6Axis_MotionApps20.h"
@@ -13,7 +20,8 @@
     Quaternion q;
     VectorFloat gravity;
     float ypr[3];
-     
+    //int t_pins[] = {4,5};
+    
     #include "Wire.h"
     extern "C" { 
     #include "utility/twi.h"  // from Wire library, so we can do bus scanning
@@ -31,124 +39,62 @@
      
      
     // standard Arduino setup()
-    void setup()
-    {
-        while (!Serial);
-        delay(1000);
-     
-        Wire.begin();
-        
-        Serial.begin(115200);
-        Serial.println("\nTCAScanner ready!");
-        
-       //for (uint8_t t=0; t<8; t++) {
-          tcaselect(7);
-          
-            Wire.begin();
-            TWBR = 24;
-            mpu.initialize();
-            mpu.dmpInitialize();
-            
-          
-            mpu.setXAccelOffset(-4232);
-            mpu.setYAccelOffset(-706);
-            mpu.setZAccelOffset(1729);
-            mpu.setXGyroOffset(173);
-            mpu.setYGyroOffset(-94);
-            mpu.setZGyroOffset(37);
-            mpu.setDMPEnabled(true);
-
-            packetSize = mpu.dmpGetFIFOPacketSize();
- 
- 
-          //Serial.print("TCA Port #"); Serial.println(t);
-     
-       /*   for (uint8_t addr = 0; addr<=127; addr++) {
-            if (addr == TCAADDR) continue;
-          
-            uint8_t data;
-            if (! twi_writeTo(addr, &data, 0, 1, 1)) {
-               Serial.print("Found I2C 0x");  Serial.println(addr,HEX);
-            }
-          }
-          
-       // }
-        Serial.println("\ndone");*/
-    }
+void setup() {
+  while (!Serial);
+    delay(1000); 
+    Wire.begin();
+    Serial.begin(115200); 
+    tcaselect(4);
+    Wire.begin();
+    TWBR = 24;
+    mpu.initialize();
+    mpu.dmpInitialize();
+    mpu.setXAccelOffset(-4232);
+    mpu.setYAccelOffset(-706);
+    mpu.setZAccelOffset(1729);
+    mpu.setXGyroOffset(173);
+    mpu.setYGyroOffset(-94);
+    mpu.setZGyroOffset(37);
+    mpu.setDMPEnabled(true);
+    packetSize = mpu.dmpGetFIFOPacketSize();
+}
      
 void loop() {
+    delay(1);
+    fifoCount = mpu.getFIFOCount();
 
-  //Código do MPU6050.
-
-  //Ждем готовоности и прерывания - ЗДЕСЬ ПОСТАВИМ ЗАДЕРЖКУ
-  delay(1);
-/*  while (!mpuInterrupt) { 
-
-    //Insira aqui o código que normalmente viria dentro da função "loop" que tudo funcionará normalmente.
-
-  }
-
-  //Código do MPU6050.
-  mpuInterrupt = false;
-*/  
-  
-  // считали данные с MPU
-  fifoCount = mpu.getFIFOCount();
-
-  if (fifoCount == 1024) {
-
-    mpu.resetFIFO();
-    Serial.println(F("FIFO overflow!"));
-
-  }
-  else {
-    //Если полученные данные меньше пакета, то задержка и считываем даные заново
-    while (fifoCount < packetSize) {
-
-     // Serial.print(fifoCount); Serial.print(" ");
-      delay(1);
-      fifoCount = mpu.getFIFOCount();
-    }
-    
-
-   // Serial.print(fifoCount); Serial.print(" * ");
-    
-    if (fifoCount % packetSize != 0) {
-//    if ((fifoCount % packetSize != 0) || (fifoCount < packetSize)) {
-
+    if (fifoCount == 1024) {
       mpu.resetFIFO();
-      Serial.print("resetFIFO");
+      // Serial.println(F("FIFO overflow!"));
     }
     else {
-
-      while (fifoCount >= packetSize) {
-
-        mpu.getFIFOBytes(fifoBuffer, packetSize);
-        fifoCount -= packetSize;
-
+      //Если полученные данные меньше пакета, то задержка и считываем даные заново
+      while (fifoCount < packetSize) {
+        // Serial.print(fifoCount); Serial.print(" ");
+        delay(1);
+        fifoCount = mpu.getFIFOCount();
+      }    
+      if (fifoCount % packetSize != 0) {
+              mpu.resetFIFO();
+        //  Serial.print("resetFIFO");
       }
-
-      mpu.dmpGetQuaternion(&q, fifoBuffer);
-      mpu.dmpGetGravity(&gravity, &q);
-      mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
-      unsigned long T;
-      static unsigned long LastT ;
-      T = millis();
-      Serial.print("ypr\t");
-      Serial.print(ypr[0] * 180 / PI);
-      Serial.print("\t");
-      Serial.print(ypr[1] * 180 / PI);
-      Serial.print("\t");
-      Serial.print(ypr[2] * 180 / PI);
-      Serial.print("\t");
- //     Serial.print(T);
-   //   Serial.print("\t");
-     // Serial.print(T - LastT);
-      Serial.println();
-      LastT = T;
-
-    }
-
+      else {
+         while (fifoCount >= packetSize) {
+           mpu.getFIFOBytes(fifoBuffer, packetSize);
+           fifoCount -= packetSize;
+         }
+         mpu.dmpGetQuaternion(&q, fifoBuffer);
+         mpu.dmpGetGravity(&gravity, &q);
+         mpu.dmpGetYawPitchRoll(ypr, &q, &gravity);
+         
+         Serial.print("n");
+        // Serial.print(NUM);
+         Serial.print(ypr[0], 4);
+         Serial.print(" ");
+         Serial.print(ypr[1], 4);
+         Serial.print(" ");
+         Serial.println(ypr[2], 4);
+         delay(10); 
+      }
   }
-
 }
